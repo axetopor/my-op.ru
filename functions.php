10b7bcc710aa448644bf4560b68a7c53b103cc45
Axe <mitya.toporkov@yandex.ru>
2025-01-31 14:23:46 +0300

    php7-update-functions

--- functions.php
+++ functions.php
@@ -1,159 +1,127 @@
 <?php
 
-function AlbumsGet($owner_id, $token, $iferr){
-	
-	@$captcha = '&captcha_sid='.$_GET ['captcha_sid'].'&captcha_key='.$_GET ['captcha_key'];
-	// echo $captcha;
-	
-	global $param;
-	global $albums_count;
-	global $album_title;
-	global $group_title;
-	global $error;
+/**
+ * Функция для получения альбомов пользователя или группы из VK API.
+ *
+ * @param int $owner_id ID владельца альбома (пользователь или группа).
+ * @param string $token Токен доступа для VK API.
+ * @param mixed $iferr Параметр для обработки ошибок (не используется в текущей реализации).
+ */
+function AlbumsGet($owner_id, $token, $iferr) {
+    // Проверяем, переданы ли параметры капчи в GET-запросе
+    $captcha = isset($_GET['captcha_sid']) && isset($_GET['captcha_key']) 
+        ? '&captcha_sid=' . $_GET['captcha_sid'] . '&captcha_key=' . $_GET['captcha_key'] 
+        : '';
 
-	
-	// if $user_id = 
-	$id = 'owner_id='.$owner_id.'&';
+    // Используем глобальные переменные для хранения результатов
+    global $param, $albums_count, $album_title, $group_title, $error;
 
-			 
-				// if (($owner_id{0})<>"-"){
-					// $id = 'owner_id=-'.$owner_id.'';
-				// } else {
-					// $id = 'owner_id='.$owner_id.'';
-				// };
+    // Формируем параметр owner_id для запроса
+    $id = 'owner_id=' . $owner_id . '&';
 
-	
-	
-	
+    // Формируем URL для запроса к VK API
+    $url = 'https://api.vk.com/method/photos.getAlbums?' . $id . '&need_covers=1&need_system=1&access_token=' . $token . $captcha;
     
-	// $json = file_get_contents('https://api.vk.com/method/photos.getAlbums?uid=-87562343&need_system=1&need_covers=1&access_token='.$token.''.$captcha );
-	$json = file_get_contents('https://api.vk.com/method/photos.getAlbums?'.$id.'&need_covers=1&need_system=1&access_token='.$token.''.$captcha );
-	$photos_arr = json_decode($json);
-	
+    // Выполняем запрос к API и декодируем JSON-ответ
+    $json = file_get_contents($url);
+    $photos_arr = json_decode($json);
 
-	@$error = $photos_arr->{'error'};
-	
-	
-	
-if(isset($error)) {
-	$err = $error->{'error_code'};
-	if ($err == 5){
-		echo "
-			Ошибка: [".$error->{'error_code'}."] <b>".$error->{'error_msg'}."</b> <br><br>
-			<img src=".$error->{'captcha_img'}.">
-			<form action='page.php?id=albums'>
-			<p>
-			<input type='text' hidden name='id' value='albums'><Br>
-			<input type='text' hidden name='captcha_sid' value=".$error->{'captcha_sid'}."><Br>
-			<input type='text' name='captcha_key' value=''><Br>
-			</p>
-			<p><input type='submit'></p>
-			</form>";
+    // Если в ответе есть ошибка, обрабатываем её
+    if (isset($photos_arr->error)) {
+        $error = $photos_arr->error;
+        $err = $error->error_code;
 
-	}
-	
-	if ($err == 3){
-		
-		$req_params = $error->{'request_params'};
-		echo $error->{'error_msg'}." <br><pre>";
-		print_r ($req_params);
-		echo "</pre>";
-	}
-	
-	if ($err == 5){
-		
-		$req_params = $error->{'request_params'};
-		echo $error->{'error_msg'}." [";
-		echo $error->{'error_code'}."] <br><pre>======= ";
-		echo $token." =======<br>";
-		print_r ($req_params);
-		echo "</pre>";
-	}	
-	if ($err == 4){
-		
-		$req_params = $error->{'request_params'};
-		echo $error->{'error_msg'}." <br><pre>";
-		print_r ($error);
-		echo "</pre>";
-	}
-	
-		if ($err == 17){
-		
-		$redirect_uri = $error->{'redirect_uri'};
-		echo "		
-		<form action=\"page.php?id=albums\" method=\"post\">
-		<button type=\"submit\" name=\"redirect_uri\" value=\".$redirect_uri.\" >Go</button>
-		</form>";
-		// echo $redirect_uri;
-	}
-	
-} else {
-	// $param[]['title'] = NULL;
-	$response = $photos_arr->{'response'};
-	$albums_count = count($response);
-	for ($i = 0; $i < $albums_count; $i++) {
+        // Обработка ошибки с кодом 5 (требуется ввод капчи)
+        if ($err == 5) {
+            echo "
+                Ошибка: [" . $error->error_code . "] <b>" . $error->error_msg . "</b> <br><br>
+                <img src=" . $error->captcha_img . ">
+                <form action='page.php?id=albums'>
+                <p>
+                <input type='text' hidden name='id' value='albums'><Br>
+                <input type='text' hidden name='captcha_sid' value=" . $error->captcha_sid . "><Br>
+                <input type='text' name='captcha_key' value=''><Br>
+                </p>
+                <p><input type='submit'></p>
+                </form>";
+        }
 
-		foreach ($response[$i] as $key=>$value) {
-			$param[$i][$key] = $value;
-			// echo $key.": ".$value."<br>";
-			
-			
-			// echo $param[$i]['id'];
-			// echo "<br>";
-			// echo $param[$i]['title'];
-			// echo "<br>";
-		}
-		$album_title[$param[$i]['aid']] = $param[$i]['title'];		
-		// echo "<hr>";
-	}
+        // Обработка ошибок с кодами 3, 4, 5 (некорректные параметры запроса)
+        if ($err == 3 || $err == 4 || $err == 5) {
+            $req_params = $error->request_params;
+            echo $error->error_msg . " <br><pre>";
+            print_r($req_params);
+            echo "</pre>";
+        }
 
-}
-		
+        // Обработка ошибки с кодом 17 (требуется подтверждение действия)
+        if ($err == 17) {
+            $redirect_uri = $error->redirect_uri;
+            echo "
+            <form action=\"page.php?id=albums\" method=\"post\">
+            <button type=\"submit\" name=\"redirect_uri\" value=\"$redirect_uri\">Go</button>
+            </form>";
+        }
+    } else {
+        // Если ошибок нет, обрабатываем ответ
+        $response = $photos_arr->response;
+        $albums_count = count($response);
+
+        // Проходим по каждому альбому и сохраняем данные
+        for ($i = 0; $i < $albums_count; $i++) {
+            foreach ($response[$i] as $key => $value) {
+                $param[$i][$key] = $value;
+            }
+            $album_title[$param[$i]['aid']] = $param[$i]['title'];
+        }
+    }
 }
 
+/**
+ * Функция для получения информации о группе по её ID.
+ *
+ * @param string $group_ids ID группы (или несколько ID через запятую).
+ */
+function groups_getById($group_ids) {
+    // Поля, которые мы хотим получить от API
+    $GIfields = "city, country, place, description, wiki_page, members_count, counters, start_date, finish_date, can_post, can_see_all_posts, activity, status, contacts, links, fixed_post, verified, site, ban_info, cover";
 
-	
-	
-	#########################################################################################
-	##	{																					#
-	##		"response": [{                              ---- $group_info->{'response'};		#
-	##			"gid": 00000000,															#
-	##			"name": "Имя заветной группы",
-	##			"screen_name": "SHORT_NAME",
-	##			"is_closed": 0,
-	##			"type": "group",
-	##			"is_admin": 0,
-	##			"is_member": 0,
-	##			"description": "Описание группы!",
-	##			"photo": "https://pp.userap...ad7/u3apuk3nKek.jpg",
-	##			"photo_medium": "https://pp.userap...ad6/jIsScXTOT5g.jpg",
-	##			"photo_big": "https://pp.userap...ad5/7E-LEsUfG4k.jpg"
-	##		}]
-	##	}																					#	
-	#########################################################################################
-	
-	
-function groups_getById($group_ids){
+    // Формируем URL для запроса к VK API
+    $url = 'https://api.vk.com/method/groups.getById?group_ids=' . $group_ids . '&fields=' . $GIfields;
+    
+    // Выполняем запрос к API и декодируем JSON-ответ
+    $group_json = file_get_contents($url);
+    $group_arr = json_decode($group_json);
 
-	
-	$GIfields = "city, country, place, description, wiki_page, members_count, counters, start_date, finish_date, can_post, can_see_all_posts, activity, status, contacts, links, fixed_post, verified, site, ban_info, cover";
-	$Gid = $group_ids;
-	
-	// $group_json = file_get_contents('https://api.vk.com/method/groups.getById?group_ids='.$Gid.'fields='.$GIfields.'&access_token='.$userRow['token'].'' );
-	$group_json = file_get_contents('https://api.vk.com/method/groups.getById?group_ids='.$Gid.'' );
-	$group_arr = json_decode($group_json);
-		//var_dump($group_arr);
-	$group_info = $group_arr->{'response'};
-		// var_dump($group_info);
-	
-	$gname = $group_info[0]->{'name'};
-	$gid = $group_info[0]->{'gid'};
-	
-	
-	
-	print $gname;
+    // Если ответ содержит данные, выводим название группы
+    if (isset($group_arr->response)) {
+        $group_info = $group_arr->response;
+        $gname = $group_info[0]->name;
+        $gid = $group_info[0]->gid;
 
-
+        print $gname;
+    } else {
+        print "Ошибка при получении информации о группе.";
+    }
 }
+
+/*
+Пример ответа от VK API для функции groups_getById:
+{
+    "response": [{
+        "gid": 00000000,
+        "name": "Имя заветной группы",
+        "screen_name": "SHORT_NAME",
+        "is_closed": 0,
+        "type": "group",
+        "is_admin": 0,
+        "is_member": 0,
+        "description": "Описание группы!",
+        "photo": "https://pp.userap...ad7/u3apuk3nKek.jpg",
+        "photo_medium": "https://pp.userap...ad6/jIsScXTOT5g.jpg",
+        "photo_big": "https://pp.userap...ad5/7E-LEsUfG4k.jpg"
+    }]
+}
+*/
 
 ?>
