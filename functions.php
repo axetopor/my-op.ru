1350099809499b48fc6a57c4be917603dbc265fb
Axe <mitya.toporkov@yandex.ru>
2025-01-31 14:29:06 +0300

    defence-form-xss-functions

--- functions.php
+++ functions.php
@@ -8,10 +8,14 @@
  * @param mixed $iferr Параметр для обработки ошибок (не используется в текущей реализации).
  */
 function AlbumsGet($owner_id, $token, $iferr) {
-    // Проверяем, переданы ли параметры капчи в GET-запросе
-    $captcha = isset($_GET['captcha_sid']) && isset($_GET['captcha_key']) 
-        ? '&captcha_sid=' . $_GET['captcha_sid'] . '&captcha_key=' . $_GET['captcha_key'] 
-        : '';
+    // Проверяем и экранируем входные данные
+    $owner_id = intval($owner_id); // Приводим к целому числу
+    $token = htmlspecialchars($token, ENT_QUOTES, 'UTF-8'); // Экранируем токен
+
+    // Проверяем, переданы ли параметры капчи в GET-запросе, и экранируем их
+    $captcha_sid = isset($_GET['captcha_sid']) ? htmlspecialchars($_GET['captcha_sid'], ENT_QUOTES, 'UTF-8') : '';
+    $captcha_key = isset($_GET['captcha_key']) ? htmlspecialchars($_GET['captcha_key'], ENT_QUOTES, 'UTF-8') : '';
+    $captcha = ($captcha_sid && $captcha_key) ? '&captcha_sid=' . $captcha_sid . '&captcha_key=' . $captcha_key : '';
 
     // Используем глобальные переменные для хранения результатов
     global $param, $albums_count, $album_title, $group_title, $error;
@@ -33,13 +37,19 @@ function AlbumsGet($owner_id, $token, $iferr) {
 
         // Обработка ошибки с кодом 5 (требуется ввод капчи)
         if ($err == 5) {
+            // Экранируем данные перед выводом
+            $error_code = htmlspecialchars($error->error_code, ENT_QUOTES, 'UTF-8');
+            $error_msg = htmlspecialchars($error->error_msg, ENT_QUOTES, 'UTF-8');
+            $captcha_img = htmlspecialchars($error->captcha_img, ENT_QUOTES, 'UTF-8');
+            $captcha_sid = htmlspecialchars($error->captcha_sid, ENT_QUOTES, 'UTF-8');
+
             echo "
-                Ошибка: [" . $error->error_code . "] <b>" . $error->error_msg . "</b> <br><br>
-                <img src=" . $error->captcha_img . ">
+                Ошибка: [$error_code] <b>$error_msg</b> <br><br>
+                <img src=\"$captcha_img\">
                 <form action='page.php?id=albums'>
                 <p>
                 <input type='text' hidden name='id' value='albums'><Br>
-                <input type='text' hidden name='captcha_sid' value=" . $error->captcha_sid . "><Br>
+                <input type='text' hidden name='captcha_sid' value=\"$captcha_sid\"><Br>
                 <input type='text' name='captcha_key' value=''><Br>
                 </p>
                 <p><input type='submit'></p>
@@ -49,14 +59,15 @@ function AlbumsGet($owner_id, $token, $iferr) {
         // Обработка ошибок с кодами 3, 4, 5 (некорректные параметры запроса)
         if ($err == 3 || $err == 4 || $err == 5) {
             $req_params = $error->request_params;
-            echo $error->error_msg . " <br><pre>";
+            $error_msg = htmlspecialchars($error->error_msg, ENT_QUOTES, 'UTF-8');
+            echo "$error_msg <br><pre>";
             print_r($req_params);
             echo "</pre>";
         }
 
         // Обработка ошибки с кодом 17 (требуется подтверждение действия)
         if ($err == 17) {
-            $redirect_uri = $error->redirect_uri;
+            $redirect_uri = htmlspecialchars($error->redirect_uri, ENT_QUOTES, 'UTF-8');
             echo "
             <form action=\"page.php?id=albums\" method=\"post\">
             <button type=\"submit\" name=\"redirect_uri\" value=\"$redirect_uri\">Go</button>
@@ -70,7 +81,8 @@ function AlbumsGet($owner_id, $token, $iferr) {
         // Проходим по каждому альбому и сохраняем данные
         for ($i = 0; $i < $albums_count; $i++) {
             foreach ($response[$i] as $key => $value) {
-                $param[$i][$key] = $value;
+                // Экранируем данные перед сохранением
+                $param[$i][$key] = is_string($value) ? htmlspecialchars($value, ENT_QUOTES, 'UTF-8') : $value;
             }
             $album_title[$param[$i]['aid']] = $param[$i]['title'];
         }
@@ -83,6 +95,9 @@ function AlbumsGet($owner_id, $token, $iferr) {
  * @param string $group_ids ID группы (или несколько ID через запятую).
  */
 function groups_getById($group_ids) {
+    // Проверяем и экранируем входные данные
+    $group_ids = htmlspecialchars($group_ids, ENT_QUOTES, 'UTF-8');
+
     // Поля, которые мы хотим получить от API
     $GIfields = "city, country, place, description, wiki_page, members_count, counters, start_date, finish_date, can_post, can_see_all_posts, activity, status, contacts, links, fixed_post, verified, site, ban_info, cover";
 
@@ -96,8 +111,8 @@ function groups_getById($group_ids) {
     // Если ответ содержит данные, выводим название группы
     if (isset($group_arr->response)) {
         $group_info = $group_arr->response;
-        $gname = $group_info[0]->name;
-        $gid = $group_info[0]->gid;
+        $gname = htmlspecialchars($group_info[0]->name, ENT_QUOTES, 'UTF-8'); // Экранируем название группы
+        $gid = htmlspecialchars($group_info[0]->gid, ENT_QUOTES, 'UTF-8'); // Экранируем ID группы
 
         print $gname;
     } else {
