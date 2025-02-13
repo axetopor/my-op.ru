eaaba42fc2049968a65553dfc2ca69d08b7d8578
Axe <mitya.toporkov@yandex.ru>
2025-02-13 22:58:52 +0300

    init-test

--- app/index.php
+++ app/index.php
@@ -66,24 +66,3 @@ echo "<b>auth_key :</b>".$auth_key."<br>";
 <a href="VK.callMethod("showSettingsBox", 8214);">ссылка</a>
 </body>
 </html>
-<!--
-?api_url=https://api.vk.com/api.php
-api_id=6229459
-api_settings=136195263
-viewer_id=105253591
-viewer_type=0
-sid=b63601dc2ac288d6e4529bc2b13dd09df85314d20154c16b1f2d73c8cbf8b96d402d74e10f27c7666a508
-secret=9de23148c1
-access_token=5ed909f702ca6f9cf478823a384629924b479e82ead45e6d302747572591b82e432186cc4fbdcd74526bc
-user_id=0
-group_id=0
-is_app_user=1
-auth_key=e0b2a01d7b13fc558479b8f2fd0f67b6
-language=0
-parent_language=0
-is_secure=1
-ads_app_id=6229459_d0aebf0e7254166642
-referrer=unknown
-lc_name=daa2800e
-hash=
---!>
